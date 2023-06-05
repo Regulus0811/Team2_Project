6405ffcc3f8274176a8843efbd1079e741a09432
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-06-05 20:44:09 +0900

    이미지 첨부 테스트중

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -11,11 +11,13 @@ export default class extends AbstractView {
       <div class="container text-center">
         <h1 class="my-4">Across Fukuoka</h1>
         <p>
-          메인화면입니다
+          대충 어떤페이지인지 소개하는 문구를 적으면 될 것 같다.
         </p>
         <p>
           <a href="/post" class="btn btn-primary" data-link>View recent posts</a>
         </p>
+        <img src="https://source.unsplash.com/random/?fukuoka" class="img-fluid" alt="Responsive image">
+        
       </div>
     `;
   }
--- frontend/static/js/views/Settings.js
+++ frontend/static/js/views/Settings.js
@@ -8,25 +8,46 @@ export default class extends AbstractView {
 
   async getHtml() {
     return `
-      <div class="container">
+      <div class="container text-center">
       <h1 class="text-center mt-5">조원 소개</h1>
       <div class="row">
         <!-- 각 조원의 카드 -->
-        <div class="col-lg-4 col-md-6">
-          <div class="card member-card">
-            <img src="/IMG_1125.png" class="card-img-top" alt="Member Image">
-            <div class="card-body">
-              <h5 class="card-title">김범창</h5>
-              <p class="card-text">프론트엔드 및 제작물내용기반 발표</p>
+        <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="card member-card mb-4">
+              <img src="" class="card-img-top" alt="Member Image">
+              <div class="card-body">
+                <h5 class="card-title">김범창</h5>
+                <p class="card-text">프론트엔드 및 제작물내용기반 발표</p>
+              </div>
+            </div>
+            <div class="card member-card">
+              <img src="" class="card-img-top" alt="Member Image">
+              <div class="card-body">
+                <h5 class="card-title">김일곤</h5>
+                <p class="card-text">백엔드 및 데이터베이스 설계</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="card member-card mb-4">
+              <img src="" class="card-img-top" alt="Member Image">
+              <div class="card-body">
+                <h5 class="card-title">김건우</h5>
+                <p class="card-text">UI/UX 디자인 및 사용자 연구</p>
+              </div>
+            </div>
+            <div class="card member-card">
+              <img src="" class="card-img-top" alt="Member Image">
+              <div class="card-body">
+                <h5 class="card-title">박수헌</h5>
+                <p class="card-text">프로젝트 매니저 및 팀 리더십</p>
+              </div>
             </div>
           </div>
         </div>
-        <!-- 추가적인 조원 카드들 -->
-        <div class="col-lg-4 col-md-6">
-          <!-- 다른 조원 카드들 추가 -->
-        </div>
       </div>
-    </div>
     `;
   }
 }
