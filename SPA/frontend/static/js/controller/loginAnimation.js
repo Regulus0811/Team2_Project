03cff86f3689c1a4884b1c44334dbdb31d66b512
lifetime909 <wjqh97@gmail.com>
2023-06-18 16:34:46 +0900

    update

--- SPA/frontend/static/js/controller/loginAnimation.js
+++ SPA/frontend/static/js/controller/loginAnimation.js
@@ -1,11 +1,36 @@
 export default function loginAnimation() {
-                // css 애니메이션 관련 js
-                window.addEventListener('load', function () {
-                    const cont = document.querySelector('.cont');
-                    cont.classList.remove('no-animation');
-                });
-                
-                document.querySelector('.img__btn').addEventListener('click', function() {
-                    document.querySelector('.cont').classList.toggle('s--signup');
-                });
-            }
+    function initializeAnimation() {
+        const cont = document.querySelector(".cont");
+        cont.classList.remove("s--signup");
+      }
+    
+      if (document.readyState === "complete") {
+        initializeAnimation();
+      } else {
+        window.addEventListener("load", initializeAnimation);
+      }
+    
+      document
+        .querySelector(".img__btn")
+        .addEventListener("click", function () {
+          const cont = document.querySelector(".cont");
+          cont.classList.toggle("s--signup");
+          // 버튼을 클릭했을 때만 애니메이션 해제
+          cont.classList.remove("no-animation");
+        });
+    
+    
+      document
+        .querySelector(".m--up")
+        .addEventListener("click", function (event) {
+          event.preventDefault();
+          document.querySelector(".cont").classList.add("s--signup");
+        });
+    
+      document
+        .querySelector(".m--in")
+        .addEventListener("click", function (event) {
+          event.preventDefault();
+          document.querySelector(".cont").classList.remove("s--signup");
+        });
+}
