8cae0a7266bda9e752ad0312f3981ab21d4d06d5
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-06-19 17:12:18 +0900

    조원소개카드 작업중

--- Team2_Backend/frontend/static/js/controller/infoController.js
+++ Team2_Backend/frontend/static/js/controller/infoController.js
@@ -1,130 +1,153 @@
 export default function infoController() {
-// 각 버튼 그룹을 선택합니다.
-const deleteBtns = document.querySelectorAll(".del_btn");
-const modifyBtns = document.querySelectorAll(".mod_btn");
-const addBtns = document.querySelectorAll(".add_btn");
+  // 각 버튼 그룹을 선택합니다.
+  const deleteBtns = document.querySelectorAll(".del_btn");
+  const modifyBtns = document.querySelectorAll(".mod_btn");
+  const addBtns = document.querySelectorAll(".add_btn");
 
-// 기본 이미지 URL을 설정합니다.
-const defaultImageUrl = "img/default.png";
+  // 기본 이미지 URL을 설정합니다.
+  const defaultImageUrl = "static/img/default.png";
 
-// 버튼 작동 업데이트 함수를 정의합니다.
-function updateButtons() {
-  // delete 버튼을 반복 처리하여 이벤트 리스너를 연결합니다.
-  deleteBtns.forEach((deleteBtn, index) => {
-    deleteBtn.addEventListener("click", () => {
-      // 해당 카드의 요소를 선택합니다.
-      const cardFrontImage = document.querySelectorAll(".Introduce-Card-Img")[
-        index
-      ];
-      const frontName = document.querySelectorAll(".Tmoney-fontsize-200")[
-        index
-      ];
-      const cardBackName = document.querySelectorAll(".Tmoney-fontsize-150")[
-        index
-      ];
-      const cardBackText = document.querySelectorAll(".KOTRA-fontsize-100")[
-        index
-      ];
+  // 버튼 작동 업데이트 함수를 정의합니다.
+  function updateButtons() {
+    // delete 버튼을 반복 처리하여 이벤트 리스너를 연결합니다.
+    deleteBtns.forEach((deleteBtn, index) => {
+      deleteBtn.addEventListener("click", () => {
+        // 해당 카드의 요소를 선택합니다.
+        const cardFrontImage = document.querySelectorAll(".Introduce-Card-Img")[
+          index
+        ];
+        const frontName = document.querySelectorAll(".Tmoney-fontsize-200")[
+          index
+        ];
+        const cardBackName = document.querySelectorAll(".Tmoney-fontsize-150")[
+          index
+        ];
+        const cardBackText = document.querySelectorAll(".KOTRA-fontsize-100")[
+          index
+        ];
 
-      // 카드의 정보를 지웁니다.
-      cardBackText.textContent = "";
-      cardBackName.textContent = "";
-      cardFrontImage.src = defaultImageUrl;
-      frontName.textContent = "";
+        // 카드의 정보를 지웁니다.
+        cardBackText.textContent = "";
+        cardBackName.textContent = "";
+        cardFrontImage.src = defaultImageUrl;
+        frontName.textContent = "";
 
-      // 버튼 상태를 업데이트합니다.
-      deleteBtn.style.display = "none";
-      modifyBtns[index].style.display = "none";
-      addBtns[index].style.display = "inline-block";
+        // 버튼 상태를 업데이트합니다.
+        deleteBtn.style.display = "none";
+        modifyBtns[index].style.display = "none";
+        addBtns[index].style.display = "inline-block";
+      });
     });
-  });
 
-  // add 버튼을 반복 처리하여 이벤트 리스너를 연결합니다.
-  addBtns.forEach((addBtn, index) => {
-    addBtn.addEventListener("click", () => {
-      // 사용자에게 새 정보를 입력하도록 요청합니다.
-      const newImageUrl = prompt("새 이미지 URL을 입력하세요:");
-      const newName = prompt("새 이름을 입력하세요:");
-      const newDescription = prompt("새 설명을 입력하세요:");
+    // add 버튼을 반복 처리하여 이벤트 리스너를 연결합니다.
+    addBtns.forEach((addBtn, index) => {
+      addBtn.addEventListener("click", () => {
+        // 모달을 열고 값 입력을 요청합니다.
+        inputModal.style.display = "block";
+        currentIndex = index;
+      });
 
-      // 해당 카드의 요소를 선택합니다.
+      // 초기 버튼 상태 설정
+      addBtn.style.display = "none";
+      deleteBtns[index].style.display = "inline-block";
+      modifyBtns[index].style.display = "inline-block";
+    });
+
+    // modift 버튼 이벤트 리스너 연결
+    modifyBtns.forEach((modifyBtn, index) => {
+      modifyBtn.addEventListener("click", () => {
+        // 모달을 열고 입력 요청
+        inputModal.style.display = "block";
+        currentIndex = index;
+
+        // 현재 카드 정보를 모달에 채웁니다.
+        const frontName = document.querySelectorAll(".Tmoney-fontsize-200")[
+          index
+        ];
+
+        newNameInput.value = frontName.textContent;
+      });
+    });
+  }
+
+  // 최초 실행하여 버튼 작동을 설정합니다.
+  updateButtons();
+
+  // 모달 관련 DOM 요소 설정
+  const inputModal = document.getElementById("inputModal");
+  const closeInputBtn = document.getElementsByClassName("close-input")[0];
+  const submitDataBtn = document.getElementById("submitData");
+
+  // 입력 필드 설정
+  const newNameInput = document.getElementById("newName");
+  const newDescriptionInput = document.getElementById("newDescription");
+
+  // 현재 클릭 된 카드 인덱스 추척
+  let currentIndex = -1;
+
+  // 제출 버튼 이벤트 리스너 설정
+  submitDataBtn.addEventListener("click", () => {
+    if (currentIndex > -1) {
+      const newImageUrl = uploadedImageUrl;
+
+      // 변수를 사용하여 DOM 요소값을 가져옵니다.
       const cardFrontImage = document.querySelectorAll(".Introduce-Card-Img")[
-        index
+        currentIndex
       ];
       const frontName = document.querySelectorAll(".Tmoney-fontsize-200")[
-        index
+        currentIndex
       ];
       const cardBackName = document.querySelectorAll(".Tmoney-fontsize-150")[
-        index
+        currentIndex
       ];
       const cardBackText = document.querySelectorAll(".KOTRA-fontsize-100")[
-        index
+        currentIndex
       ];
 
-      // 카드에 입력한 정보를 반영합니다.
       cardFrontImage.src = newImageUrl;
-      cardBackName.textContent = newName;
-      frontName.textContent = newName;
-      cardBackText.innerHTML = newDescription;
+      cardBackName.textContent = newNameInput.value;
+      frontName.textContent = newNameInput.value;
+      cardBackText.innerHTML = newDescriptionInput.value;
 
       // 버튼 상태를 업데이트합니다.
-      addBtn.style.display = "none";
-      deleteBtns[index].style.display = "inline-block";
-      modifyBtns[index].style.display = "inline-block";
-    });
+      deleteBtns[currentIndex].style.display = "inline-block";
+      modifyBtns[currentIndex].style.display = "inline-block";
+      addBtns[currentIndex].style.display = "none";
 
-    // 초기 버튼 상태 설정
-    if (index === 0) {
-      addBtn.style.display = "none";
-      deleteBtns[index].style.display = "inline-block";
-      modifyBtns[index].style.display = "inline-block";
-    } else {
-      addBtn.style.display = "inline-block";
-      deleteBtns[index].style.display = "none";
-      modifyBtns[index].style.display = "none";
+      // 입력 필드를 초기화합니다.
+      newNameInput.value = "";
+      newDescriptionInput.value = "";
+      uploadedImageUrl = ""; // 이미지 URL 초기화
+
+      currentIndex = -1;
     }
-    // 초기 버튼 상태 설정
-    addBtn.style.display = "none";
-    deleteBtns[index].style.display = "inline-block";
-    modifyBtns[index].style.display = "inline-block";
+
+    // 입력 모달을 닫습니다.
+    inputModal.style.display = "none";
   });
 
-  // modify 버튼을 반복 처리하여 이벤트 리스너를 연결합니다.
-  modifyBtns.forEach((modifyBtn, index) => {
-    modifyBtn.addEventListener("click", () => {
-      // 사용자에게 새 정보를 입력하도록 요청합니다.
-      const newImageUrl = prompt("새 이미지 URL을 입력하세요:");
-      const newName = prompt("새 이름을 입력하세요:");
-      const newDescription = prompt("새 설명을 입력하세요:");
+  // 'X'를 클릭하면 입력 모달이 닫힙니다.
+  closeInputBtn.onclick = () => {
+    inputModal.style.display = "none";
+  };
 
-      // 해당 카드의 요소를 선택합니다.
-      const cardFrontImage = document.querySelectorAll(".Introduce-Card-Img")[
-        index
-      ];
-      const frontName = document.querySelectorAll(".Tmoney-fontsize-200")[
-        index
-      ];
-      const cardBackName = document.querySelectorAll(".Tmoney-fontsize-150")[
-        index
-      ];
-      const cardBackText = document.querySelectorAll(".KOTRA-fontsize-100")[
-        index
-      ];
+  // 이미지 업로드를 위한 DOM 요소 설정 및 변수
+  const newImageFile = document.getElementById("newImageFile");
+  let uploadedImageUrl = "";
 
-      // 카드에 입력한 정보를 반영합니다.
-      if (newImageUrl) {
-        cardFrontImage.src = newImageUrl;
-      }
-      if (newName) {
-        cardBackName.textContent = newName;
-      }
-      if (newDescription) {
-        cardBackText.innerHTML = newDescription;
-      }
-    });
-  });
-}
+  // 이미지 파일 업로드 이벤트 탐지
+  newImageFile.addEventListener("change", readImageFile);
 
-// 최초 실행하여 버튼 작동을 설정합니다.
-updateButtons();
+  // 파일 업로드 후 이미지 URL 저장
+  function readImageFile(event) {
+    const imageFile = event.target.files[0];
+
+    if (imageFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        uploadedImageUrl = reader.result;
+      };
+      reader.readAsDataURL(imageFile);
+    }
+  }
 }
--- Team2_Backend/frontend/static/js/pages/Info.js
+++ Team2_Backend/frontend/static/js/pages/Info.js
@@ -1,9 +1,9 @@
 export default class {
-    constructor() {
-        document.title = "PersonInfo";
-    }
-    async getHtml() {
-        return `
+  constructor() {
+    document.title = "PersonInfo";
+  }
+  async getHtml() {
+    return `
         <link rel="stylesheet" href="static/css/info.css">
         <!-- 조원소개 제목 -->
         <div class="card">
@@ -15,217 +15,56 @@ export default class {
         </div>
     
         <!-- 조원소개 카드 -->
-        <!--플립 카드 시작-->
-        <div class="container">
-          <div class="align-Center-row">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <img
-                    src="img/pepe.jpg"
-                    class="Introduce-Card-Img"
-                    alt="introduce-repick-icon"
-                  />
-                  <h1 class="Tmoney-fontsize-200">김범창</h1>
-                </div>
-                <div class="flip-card-back">
-                  <h1 class="Tmoney-fontsize-150 MarginBottom-5">김범창</h1>
-                  <p class="KOTRA-fontsize-100">
-                    <br />프론트를 담당하여 <br />메인페이지 및 조원소개페이지를
-                    <br />제작하였습니다
-                  </p>
-    
-                  <!-- 버튼 -->
-                  <div class="container" id="main_box">
-                    <div class="container_header"></div>
-                    <div class="plans_container">
-                      <div class="row">
-                        <div class="plans_header col-12">
-                          <ul class="plans_header_ul"></ul>
-                        </div>
-    
-                        <!-- 삭제,수정,추가 버튼 -->
-                        <div class="btn_box row-cols-12">
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 del_btn"
-                          >
-                            Delete
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 mod_btn"
-                          >
-                            Modify
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 add_btn"
-                            style="display: none"
-                          >
-                            Add
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <!--플립 카드 시작-->
+    <div class="container">
+      <div class="align-Center-row">
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="static/img/pepe.jpg"
+                class="Introduce-Card-Img"
+                alt="introduce-repick-icon"
+              />
+              <h1 class="Tmoney-fontsize-200">김범창</h1>
             </div>
-    
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <img
-                    src="img/pepe2.jpg"
-                    class="Introduce-Card-Img"
-                    alt="introduce-repick-icon"
-                  />
-                  <h1 class="Tmoney-fontsize-200">김일곤</h1>
-                </div>
-                <div class="flip-card-back">
-                  <h1 class="Tmoney-fontsize-150 MarginBottom-5">김일곤</h1>
-                  <p class="KOTRA-fontsize-100">
-                    백을 담당하여 <br />로그인 기능과 데이터관리를
-                    <br />구현하였습니다
-                  </p>
-                  <!-- 버튼 -->
-                  <div class="container" id="main_box">
-                    <div class="container_header"></div>
-                    <div class="plans_container">
-                      <div class="row">
-                        <div class="plans_header col-12">
-                          <ul class="plans_header_ul"></ul>
-                        </div>
-    
-                        <!-- 삭제,수정,추가 버튼 -->
-                        <div class="btn_box row-cols-12">
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 del_btn"
-                          >
-                            Delete
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 mod_btn"
-                          >
-                            Modify
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 add_btn"
-                            style="display: none"
-                          >
-                            Add
-                          </button>
-                        </div>
-                      </div>
+            <div class="flip-card-back">
+              <h1 class="Tmoney-fontsize-150 MarginBottom-5">김범창</h1>
+              <p class="KOTRA-fontsize-100">
+                <br />프론트를 담당하여 <br />메인페이지 및 조원소개페이지를
+                <br />제작하였습니다
+              </p>
+
+              <!-- 버튼 -->
+              <div class="container" id="main_box">
+                <div class="container_header"></div>
+                <div class="plans_container">
+                  <div class="row">
+                    <div class="plans_header col-12">
+                      <ul class="plans_header_ul"></ul>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-    
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <img
-                    src="img/pepe3.jpg"
-                    class="Introduce-Card-Img"
-                    alt="introduce-repick-icon"
-                  />
-                  <h1 class="Tmoney-fontsize-200">김건우</h1>
-                </div>
-                <div class="flip-card-back">
-                  <h1 class="Tmoney-fontsize-150 MarginBottom-5">김건우</h1>
-                  <p class="KOTRA-fontsize-100">
-                    백을 담당하여 <br />로그인 기능 및 데이터관리를
-                    <br />구현하였습니다
-                  </p>
-                  <!-- 버튼 -->
-                  <div class="container" id="main_box">
-                    <div class="container_header"></div>
-                    <div class="plans_container">
-                      <div class="row">
-                        <div class="plans_header col-12">
-                          <ul class="plans_header_ul"></ul>
-                        </div>
-    
-                        <!-- 삭제,수정,추가 버튼 -->
-                        <div class="btn_box row-cols-12">
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 del_btn"
-                          >
-                            Delete
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 mod_btn"
-                          >
-                            Modify
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 add_btn"
-                          >
-                            Add
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-    
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <img
-                    src="img/pepe4.jpg"
-                    class="Introduce-Card-Img"
-                    alt="introduce-repick-icon"
-                  />
-                  <h1 class="Tmoney-fontsize-200">박수헌</h1>
-                </div>
-                <div class="flip-card-back">
-                  <h1 class="Tmoney-fontsize-150 MarginBottom-5">박수헌</h1>
-                  <p class="KOTRA-fontsize-100">
-                    프론트를 담당하여 <br />게시판 페이지를 <br />제작하였습니다
-                  </p>
-                  <!-- 버튼 -->
-                  <div class="container" id="main_box">
-                    <div class="container_header"></div>
-                    <div class="plans_container">
-                      <div class="row">
-                        <div class="plans_header col-12">
-                          <ul class="plans_header_ul"></ul>
-                        </div>
-    
-                        <!-- 삭제,수정,추가 버튼 -->
-                        <div class="btn_box row-cols-12">
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 del_btn"
-                          >
-                            Delete
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 mod_btn"
-                          >
-                            Modify
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-primary col-1 add_btn"
-                          >
-                            Add
-                          </button>
-                        </div>
-                      </div>
+
+                    <!-- 삭제,수정,추가 버튼 -->
+                    <div class="btn_box row-cols-12">
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 del_btn"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 mod_btn"
+                      >
+                        Modify
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 add_btn"
+                        style="display: none"
+                      >
+                        Add
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -233,14 +72,190 @@ export default class {
             </div>
           </div>
         </div>
-    
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-          crossorigin="anonymous"
-        ></script>
-        <script src="js/intord2.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="static/img/pepe2.jpg"
+                class="Introduce-Card-Img"
+                alt="introduce-repick-icon"
+              />
+              <h1 class="Tmoney-fontsize-200">김일곤</h1>
+            </div>
+            <div class="flip-card-back">
+              <h1 class="Tmoney-fontsize-150 MarginBottom-5">김일곤</h1>
+              <p class="KOTRA-fontsize-100">
+                백을 담당하여 <br />로그인 기능과 데이터관리를
+                <br />구현하였습니다
+              </p>
+              <!-- 버튼 -->
+              <div class="container" id="main_box">
+                <div class="container_header"></div>
+                <div class="plans_container">
+                  <div class="row">
+                    <div class="plans_header col-12">
+                      <ul class="plans_header_ul"></ul>
+                    </div>
+
+                    <!-- 삭제,수정,추가 버튼 -->
+                    <div class="btn_box row-cols-12">
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 del_btn"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 mod_btn"
+                      >
+                        Modify
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 add_btn"
+                        style="display: none"
+                      >
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="static/img/pepe3.jpg"
+                class="Introduce-Card-Img"
+                alt="introduce-repick-icon"
+              />
+              <h1 class="Tmoney-fontsize-200">김건우</h1>
+            </div>
+            <div class="flip-card-back">
+              <h1 class="Tmoney-fontsize-150 MarginBottom-5">김건우</h1>
+              <p class="KOTRA-fontsize-100">
+                백을 담당하여 <br />로그인 기능 및 데이터관리를
+                <br />구현하였습니다
+              </p>
+              <!-- 버튼 -->
+              <div class="container" id="main_box">
+                <div class="container_header"></div>
+                <div class="plans_container">
+                  <div class="row">
+                    <div class="plans_header col-12">
+                      <ul class="plans_header_ul"></ul>
+                    </div>
+
+                    <!-- 삭제,수정,추가 버튼 -->
+                    <div class="btn_box row-cols-12">
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 del_btn"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 mod_btn"
+                      >
+                        Modify
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 add_btn"
+                      >
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img
+                src="static/img/pepe4.jpg"
+                class="Introduce-Card-Img"
+                alt="introduce-repick-icon"
+              />
+              <h1 class="Tmoney-fontsize-200">박수헌</h1>
+            </div>
+            <div class="flip-card-back">
+              <h1 class="Tmoney-fontsize-150 MarginBottom-5">박수헌</h1>
+              <p class="KOTRA-fontsize-100">
+                프론트를 담당하여 <br />게시판 페이지를 <br />제작하였습니다
+              </p>
+              <!-- 버튼 -->
+              <div class="container" id="main_box">
+                <div class="container_header"></div>
+                <div class="plans_container">
+                  <div class="row">
+                    <div class="plans_header col-12">
+                      <ul class="plans_header_ul"></ul>
+                    </div>
+
+                    <!-- 삭제,수정,추가 버튼 -->
+                    <div class="btn_box row-cols-12">
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 del_btn"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 mod_btn"
+                      >
+                        Modify
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-primary col-1 add_btn"
+                      >
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 입력을 위한 모달 추가 -->
+    <div id="inputModal" class="modal">
+      <div class="modal-content">
+        <span class="close-input">&times;</span>
+        <label for="newImageFile">이미지 파일을 업로드하세요:</label>
+        <input type="file" id="newImageFile" accept="image/*" />
+        <label for="newName">새 이름을 입력하세요:</label>
+        <input type="text" id="newName" name="newName" />
+        <label for="newDescription">새 설명을 입력하세요:</label>
+        <textarea id="newDescription" name="newDescription"></textarea>
+        <button id="submitData">제출</button>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+      crossorigin="anonymous"
+    ></script>
+    <script src="js/intord2.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
         `;
-    }
+  }
 }
